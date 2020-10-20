04c1d251dabb3ed9220b97ebadc7e53a087a6429
Alex Zinkevych <alexdarkmm@gmail.com>
2020-10-21 00:49:32 +0300

    Added projects page plug

--- src/routes/Projects/Projects.tsx
+++ src/routes/Projects/Projects.tsx
@@ -1,26 +1,9 @@
 import React from "react";
 
-import { CodeLikeBlock, L } from "~/components/CodeLikeBlock";
-import { useSkillsContext } from "~/components/SkillsProvider";
-
-import { useStyles } from "./Projects.styles";
+import { CodeLikeBlock } from "~/components/CodeLikeBlock";
 
 export const Projects: React.FC = () => {
-  const {
-    selectedSkills,
-    unselectSkill,
-    clearSelectedSkills,
-  } = useSkillsContext();
-  return (
-    <CodeLikeBlock>
-      {selectedSkills.map((skill) => (
-        <L key={skill} onClick={() => unselectSkill(skill)}>
-          {skill}
-        </L>
-      ))}
-      <L onClick={clearSelectedSkills}>Clear</L>
-    </CodeLikeBlock>
-  );
+  return <CodeLikeBlock>Under development...</CodeLikeBlock>;
 };
 
 export default Projects;
