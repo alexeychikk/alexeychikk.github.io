bd4f9d4184af8a86f1267ba81d5e2cf653959d22
Alex Zinkevych <alexdarkmm@gmail.com>
2021-04-15 14:45:15 +0300

    Removed projects due to NDA

--- src/routes/Projects/Projects.tsx
+++ src/routes/Projects/Projects.tsx
@@ -3,7 +3,9 @@ import React from "react";
 import { CodeLikeBlock } from "~/components/CodeLikeBlock";
 
 export const Projects: React.FC = () => {
-  return <CodeLikeBlock>Under development...</CodeLikeBlock>;
+  return (
+    <CodeLikeBlock>This section was removed because of NDA-s :(</CodeLikeBlock>
+  );
 };
 
 export default Projects;
