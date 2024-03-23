cc315403b90d7040574e46113eafec2d6da06832
Alex Zinkevych <alexdarkmm@gmail.com>
2024-03-23 18:21:15 +0200

    fix: better responsive styles

--- src/routes/AboutMe.svelte
+++ src/routes/AboutMe.svelte
@@ -1,1 +1,7 @@
-<div>About ME</div>
+<div>
+  {`Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam ratione
+  deserunt unde ut architecto. Eveniet non quod voluptate odit vel quidem modi.
+  Asperiores obcaecati qui minima perspiciatis nisi quidem architecto.`.repeat(
+    100,
+  )}
+</div>
