33f3d3f36eb0e29e085f7d216d5b6b9752fcb9fe
Alex Zinkevych <alexdarkmm@gmail.com>
2024-03-30 18:14:15 +0200

    chore: added my own vite total bundle size plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { analyzer } from 'vite-bundle-analyzer';
 import svgr from 'vite-plugin-svgr';
+import { totalBundleSize } from 'vite-plugin-total-bundle-size';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
@@ -11,6 +12,7 @@ export default defineConfig(({ command }) => ({
     react(),
     svgr(),
     command === 'build' && analyzer({ analyzerMode: 'static' }),
+    command === 'build' && totalBundleSize(),
   ],
   base: '',
   build: { outDir: 'docs' },
