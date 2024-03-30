9b75a236ef85b2e59f3366f489402f55371302dd
Alex Zinkevych <alexdarkmm@gmail.com>
2024-03-30 17:07:34 +0200

    chore: added my own vite total bundle size plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import svg from '@poppanator/sveltekit-svg';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
 import { analyzer } from 'vite-bundle-analyzer';
+import { totalBundleSize } from 'vite-plugin-total-bundle-size';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
@@ -23,6 +24,7 @@ export default defineConfig(({ command }) => ({
       },
     }),
     command === 'build' && analyzer({ analyzerMode: 'static' }),
+    command === 'build' && totalBundleSize(),
   ],
   base: '',
   build: { outDir: 'docs' },
